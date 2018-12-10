5d4084952cbf227a51b339d85e8e905a6f6bdf81
njh2wj <45623538+njh2wj@users.noreply.github.com>
2018-12-10 13:33:47 +0800

    Update informationform.component.ts

--- src/app/informationform/informationform.component.ts
+++ src/app/informationform/informationform.component.ts
@@ -9,17 +9,14 @@ import {isNullOrUndefined} from "util";
   styleUrls: ['./informationform.component.css']
 })
 
-
 export class InformationformComponent implements OnInit {
 
   @Input() public person: Person = new Person();
-
 
   constructor() {
   }
 
   @Output() send = new EventEmitter<boolean>();
-
 
   ngOnInit() {
     const person = localStorage.getItem('personEntity');
@@ -33,17 +30,45 @@ export class InformationformComponent implements OnInit {
     var value = event.target.value;
     this.person.usex = Number(value);
   }
+  unameVal(event){
+    var value=event.target.value;
+    const reg= /^([a-zA-Z0-9\u4e00-\u9fa5\·]{1,10})$/;
+    if(reg.test(value)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  ageVal(event){
+    var value=event.target.value;
+    const reg= /^(?:[1-9][0-9]?|1[01][0-9]|120)$/;
+    if(reg.test(value)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  cancel(){
+    const person = JSON.parse(localStorage.getItem('personEntity'));
+    if(!(person===this.person)) {
+      alert("信息已更改，未保存！");
+    }
+    this.unsave(false);
+  }
+
+  public unsave(val){
+    this.send.emit(val);
+   }
 
   public save(a: boolean) {
     if (!isNullOrUndefined(this.person.uname) && !isNullOrUndefined(this.person.uage) && !isNullOrUndefined(this.person.usex) && !isNullOrUndefined(this.person.utel) && !isNullOrUndefined(this.person.uaddress)) {
       localStorage.setItem('personEntity', JSON.stringify(this.person));
-      this.send.emit(!a)
+      this.send.emit(!a);
     }
     else {
       alert("请补全信息！");
       return;
     }
-
-
   }
 }
