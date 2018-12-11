9f4de5c1c1539b13618b540d04a7eeb57a17034e
njh2wj <45623538+njh2wj@users.noreply.github.com>
2018-12-11 17:39:08 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InforDetailComponent } from './infor-detail/infor-detail.component';
+import { IndexComponent } from './index/index.component';
+import { PersondetailComponent } from './persondetail/persondetail.component';
+import {InformationformComponent} from './informationform/informationform.component';
 
-const routes: Routes = [];
-
+// const routes: Routes = [];
+const appRoutes: Routes = <Routes>[
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full',
+  },
+  {
+    path: 'persondetail',
+    component: PersondetailComponent
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'informationform',
+    component: InformationformComponent
+  },
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
